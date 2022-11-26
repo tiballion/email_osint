29f91539eb9af7bfd49549c6156ba04a1cd1e289
tiballion <timothee.ballion.33@gmail.com>
2022-11-27 00:02:45 +0100

    changed email.py name to email_check.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from tools.instagram import instagram
 from tools.discord import discord
 from tools.twitter import twitter
 from tools.spotify import spotify
-from tools.email import is_valid_email
+from tools.email_check import is_valid_email
 
 
 def header():
@@ -23,16 +23,15 @@ def header():
 
 
 def main():
-    # header()
-    # email = input("Enter the email: ")
-    # if is_valid_email(email):
-        # res = [instagram(email), discord(email), twitter(email), spotify(email)]
-        # for r in res:
-            # print(r)
-    # else:
-        # print("Invalid email!")
-        # exit()
-    print(spotify("test@fdp.com"))
+    header()
+    email = input("Enter the email: ")
+    if is_valid_email(email):
+        res = [instagram(email), discord(email), twitter(email), spotify(email)]
+        for r in res:
+            print(r)
+    else:
+        print("Invalid email!")
+        exit()
 
 
 if __name__ == "__main__":
