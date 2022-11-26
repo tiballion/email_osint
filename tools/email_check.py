c65d5bd045df52c2f70f10f742cc9a1b533943c8
tiballion <timothee.ballion.33@gmail.com>
2022-11-27 00:28:10 +0100

    refactored email_check.py, refactored regex in email_check.py

--- tools/email_check.py
+++ tools/email_check.py
@@ -2,7 +2,4 @@ import re
 
 
 def is_valid_email(email):
-    if re.match(r"^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$", email):
-        return True
-    else:
-        return False
+    return re.match(r"^[a-z0-9]+[._]?[a-z0-9]+@\w+[.]\w{2,3}$", email)
